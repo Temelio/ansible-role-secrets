ad3d4ac994b5d64c0a2070883f4b5a1602febc2e
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2016-04-26 00:45:46 +0200

    Test including sudo in gpg list keys command

--- spec/gpg_public_keys_management_spec.rb
+++ spec/gpg_public_keys_management_spec.rb
@@ -10,7 +10,7 @@ describe 'secrets Ansible role - SSH private keys' do
     present_gpg_keys = Array[ '2048R/3488F25B', '2048R/D1CE33EC' ]
     absent_gpg_keys = Array[ '7D3DD982' ]
 
-    describe command('gpg --list-keys') do
+    describe command('sudo -u root gpg --list-keys') do
 
         present_gpg_keys.each do |key_id|
             its(:stdout) { should match /.*#{key_id}.*/ }
